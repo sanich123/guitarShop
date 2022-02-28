558088b75d7a68d7e8e3e9988b3f2ebe3de1e88d
sanich123 <aovoronin.piano@gmail.com>
2022-02-28 13:02:48 +0300

    some small refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-// import './public/css/style.css';
+import './public/css/style.css';
 
 ReactDOM.render(
   <React.StrictMode>
