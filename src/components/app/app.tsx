3af3feaf2e9173d2d47f58b14a116c78de046ac6
sanich123 <aovoronin.piano@gmail.com>
2022-02-28 12:47:24 +0300

    Initial commit

---
+++ src/components/app/app.tsx
@@ -0,0 +1,6 @@
+import Main from '../main/main';
+
+export default function App(): JSX.Element {
+  return <Main />;
+}
+
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+// import './public/css/style.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
