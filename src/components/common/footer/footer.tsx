1ee9bc60ec47c5eb6c5847b3aa4371991ae148e5
Alexander Voronin <aovoronin.piano@gmail.com>
2022-04-17 23:07:07 +0300

    Some fixings

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -9,7 +9,7 @@ function Footer() {
     <footer className="footer">
       <div className="footer__container container">
         <Link className="footer__logo logo" to={appRoutes.main}>
-          <img className="logo__img" width="70" height="70" src="./img/svg/logo.svg" alt="Логотип"/>
+          <img className="logo__img" width="70" height="70" src="../img/svg/logo.svg" alt="Логотип"/>
         </Link>
         <div className="socials footer__socials">
           <Social />
