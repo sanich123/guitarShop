f4e0b422683138d33166ea501b10400015a27969
sanich123 <aovoronin.piano@gmail.com>
2022-04-30 13:13:49 +0300

    Migrating to react18

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import { Provider } from 'react-redux';
 import { setupStore } from '../src/redux/store';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const container = document.getElementById('root') as Element;
+const root = createRoot(container);
+root.render(
   <Provider store={setupStore()}>
     <ToastContainer />
     <App />
   </Provider>,
-  document.getElementById('root'),
 );
 
