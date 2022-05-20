acc6cd46703e7f52ef62c7ebb4593156ccb5bafb
sanich123 <aovoronin.piano@gmail.com>
2022-05-20 16:16:56 +0300

    Done with price inputs

--- src/components/catalog/filters-price/input-max-price.tsx
+++ src/components/catalog/filters-price/input-max-price.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { useGetGuitarsQuery } from '../../../redux/guitars-api';
-import { FiltersProps, Guitar } from '../../../types/types';
+import { FiltersProps } from '../../../types/types';
 import { priceWarnings, searchParams } from '../../../utils/const';
 import { getDefaultMaxValue, getDefaultMinValue } from '../../../utils/utils';
 
@@ -10,7 +10,7 @@ interface InputMaxPriceProps extends FiltersProps {
   setFilterMaxPrice: (arg: string) => void;
 }
 
-export default function InputMaxPrice({setFilterMaxPrice, setPageNumber, guitars, isError, needToReset, setNeedToReset}: InputMaxPriceProps) {
+export default function InputMaxPrice({setFilterMaxPrice, setPageNumber, isError, needToReset, setNeedToReset}: InputMaxPriceProps) {
   const { search } = useLocation();
   const params = new URLSearchParams(search);
   const filterMaxPrice = params.get(searchParams.maxPrice);
@@ -19,8 +19,6 @@ export default function InputMaxPrice({setFilterMaxPrice, setPageNumber, guitars
   const { data: defaultGuitars } = useGetGuitarsQuery('');
   const biggestPrice = defaultGuitars ? getDefaultMaxValue(defaultGuitars) : 0;
   const smallestPrice = defaultGuitars ? getDefaultMinValue(defaultGuitars) : 0;
-  const filtredPrices = guitars?.map(({ price }: Guitar) => price);
-  const maxPrice = Math.max(...filtredPrices);
 
   useEffect(() => {
     if (needToReset) {
@@ -32,34 +30,46 @@ export default function InputMaxPrice({setFilterMaxPrice, setPageNumber, guitars
 
   const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
     setPageNumber(1);
-    const price = Math.abs(Number(target.value));
     const value = target.value;
+    const price = Math.abs(Number(value));
     setPrice(value);
     if (value[0] === '0' || value.slice(0, 2) === '-0') {
       toast.warn(priceWarnings.zeroNum);
       setPrice('');
       return;
     }
-    if (price > smallestPrice && price > Number(filterMinPrice) && price < biggestPrice) {
-      setFilterMaxPrice(`${searchParams.maxPrice}=${Math.abs(+value)}`);
-    } else {
-      toast.warn(priceWarnings.smallerAndBigger);
+    if (price >= smallestPrice && price <= biggestPrice) {
+      if (!filterMinPrice || (filterMinPrice &&
+      price >= Number(filterMinPrice))) {
+        setFilterMaxPrice(`${searchParams.maxPrice}=${price}`);
+        setPrice(`${price}`);
+      }
+      if (filterMinPrice && price < Number(filterMinPrice)) {
+        toast.warn(priceWarnings.currentSmallerThanMin);
+      }
     }
   };
 
   const handleBlur = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
     setPageNumber(1);
     const price = Math.abs(Number(target.value));
-    if (!price) {return;}
-    if (price > biggestPrice) {
-      toast.warn(priceWarnings.biggerThanMax);
+    if (!price) {
+      setFilterMaxPrice('');
+      return;
+    }
+    if (
+      price < smallestPrice ||
+      price > biggestPrice ||
+      (filterMinPrice && price < Number(filterMinPrice))
+    ) {
+      toast.warn(priceWarnings.maxWarning);
       setPrice(`${biggestPrice}`);
       setFilterMaxPrice(`${searchParams.maxPrice}=${biggestPrice}`);
     }
-    if (price < Number(filterMinPrice) || price < smallestPrice) {
-      toast.warn(priceWarnings.smallerThanTypedMin);
-      setPrice(`${biggestPrice}`);
-      setFilterMaxPrice(`${searchParams.maxPrice}=${biggestPrice}`);
+    if (price < biggestPrice && price > smallestPrice) {
+      if (!filterMinPrice || (filterMinPrice && price > Number(filterMinPrice))) {
+        setFilterMaxPrice(`${searchParams.maxPrice}=${price}`);
+      }
     }
   };
 
@@ -75,7 +85,7 @@ export default function InputMaxPrice({setFilterMaxPrice, setPageNumber, guitars
       <label className="visually-hidden">Максимальная цена</label>
       <input
         type="number"
-        placeholder={maxPrice.toLocaleString('ru-Ru')}
+        placeholder={biggestPrice.toLocaleString('ru-Ru')}
         id="priceMax"
         name="до"
         value={priceValue}
--- src/components/catalog/filters-price/input-min-price.tsx
+++ src/components/catalog/filters-price/input-min-price.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { useGetGuitarsQuery } from '../../../redux/guitars-api';
-import { FiltersProps, Guitar } from '../../../types/types';
+import { FiltersProps } from '../../../types/types';
 import { priceWarnings, searchParams } from '../../../utils/const';
 import { getDefaultMaxValue, getDefaultMinValue } from '../../../utils/utils';
 
@@ -10,18 +10,15 @@ interface InputMinPriceProps extends FiltersProps {
   setFilterMinPrice: (arg: string) => void;
 }
 
-export default function InputMinPrice({setFilterMinPrice, guitars,isError, setPageNumber, needToReset,
-  setNeedToReset}: InputMinPriceProps) {
+export default function InputMinPrice({setFilterMinPrice, isError, setPageNumber, needToReset, setNeedToReset}: InputMinPriceProps) {
   const { search } = useLocation();
   const params = new URLSearchParams(search);
   const filterMinPrice = params.get(searchParams.minPrice);
+  const filterMaxPrice = params.get(searchParams.maxPrice);
   const [priceValue, setPrice] = useState(filterMinPrice ? filterMinPrice : '');
-
   const { data: defaultGuitars } = useGetGuitarsQuery('');
   const smallestPrice = defaultGuitars ? getDefaultMinValue(defaultGuitars) : 0;
   const biggestPrice = defaultGuitars ? getDefaultMaxValue(defaultGuitars) : 0;
-  const filtredPrices = guitars?.map(({ price }: Guitar) => price);
-  const minPrice = Math.min(...filtredPrices);
 
   useEffect(() => {
     if (needToReset) {
@@ -33,40 +30,52 @@ export default function InputMinPrice({setFilterMinPrice, guitars,isError, setPa
 
   const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
     setPageNumber(1);
-    const price = Math.abs(Number(target.value));
-    setPrice(target.value);
-    if (target.value[0] === '0' || target.value.slice(0, 2) === '-0') {
+    const value = target.value;
+    const price = Math.abs(Number(value));
+    setPrice(value);
+    if (value[0] === '0' || value.slice(0, 2) === '-0') {
       toast.warn(priceWarnings.zeroNum);
       setPrice('');
       return;
     }
-    if (price > smallestPrice && price < biggestPrice) {
-      setFilterMinPrice(`${searchParams.minPrice}=${Math.abs(Number(target.value))}`);
-      setPrice(`${price}`);
-    } else {
-      toast.warn(priceWarnings.smallerAndBigger);
+    if (price >= smallestPrice && price <= biggestPrice) {
+      if (!filterMaxPrice || (filterMaxPrice && price < Number(filterMaxPrice))) {
+        setFilterMinPrice(`${searchParams.minPrice}=${price}`);
+        setPrice(`${price}`);
+      }
+      if (filterMaxPrice && price > Number(filterMaxPrice)) {
+        toast.warn(priceWarnings.currentBiggerThanMax);
+      }
     }
   };
 
   const handleBlur = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
     setPageNumber(1);
     const price = Math.abs(Number(target.value));
-    if (!price) {return;}
-    if (price < smallestPrice) {
-      toast.warn(priceWarnings.smallerThanMin);
+    if (!price) {
+      setFilterMinPrice('');
+      return;
+    }
+    if (
+      price < smallestPrice ||
+      price > biggestPrice ||
+      (filterMaxPrice && price > Number(filterMaxPrice))
+    ) {
+      toast.warn(priceWarnings.minWarning);
       setPrice(`${smallestPrice}`);
       setFilterMinPrice(`${searchParams.minPrice}=${smallestPrice}`);
     }
-    if (price > biggestPrice) {
-      toast.warn(priceWarnings.biggerThanMax);
-      setPrice(`${biggestPrice}`);
-      setFilterMinPrice(`${searchParams.minPrice}=${biggestPrice}`);
+    if (price < biggestPrice && price > smallestPrice) {
+      if (!filterMaxPrice || (filterMaxPrice && price < Number(filterMaxPrice))) {
+        setFilterMinPrice(`${searchParams.minPrice}=${price}`);
+      }
     }
   };
 
   const handleKeyDown = ({ code }: React.KeyboardEvent<HTMLInputElement>) => {
     if (code === 'KeyE') {
       toast.warn(priceWarnings.typeCharE);
+      setFilterMinPrice('');
     }
   };
 
@@ -75,7 +84,7 @@ export default function InputMinPrice({setFilterMinPrice, guitars,isError, setPa
       <label className="visually-hidden">Минимальная цена</label>
       <input
         type="number"
-        placeholder={minPrice.toLocaleString('ru-Ru')}
+        placeholder={smallestPrice.toLocaleString('ru-Ru')}
         id="priceMin"
         name="от"
         value={priceValue}
