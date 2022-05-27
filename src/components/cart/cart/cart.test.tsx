7e8d8bfe87490ea97710e89ad8fb996932586523
sanich123 <aovoronin.piano@gmail.com>
2022-05-27 20:13:53 +0300

    Some refactoring with cart

--- src/components/cart/cart/cart.test.tsx
+++ src/components/cart/cart/cart.test.tsx
@@ -1,18 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-router-dom';
-import { setupStore } from '../../../redux/store';
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from '../../../tests/test-utils';
 import Cart from './cart';
 
 describe('Cart', () => {
   it('Cart should render properly', async () => {
-    render(
-      <Provider store={setupStore()}>
-        <MemoryRouter>
-          <Cart />
-        </MemoryRouter>
-      </Provider>,
-    );
+    renderWithProviders(<Cart />);
     expect(screen.getByText(/loading/i)).toBeInTheDocument();
     expect(await screen.findByText(/Your cart has no items/i)).toBeInTheDocument();
   });
