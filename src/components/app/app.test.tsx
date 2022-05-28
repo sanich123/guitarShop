6abfe0100b204e719b085f2d4703b36eaf7bb458
sanich123 <aovoronin.piano@gmail.com>
2022-05-28 16:29:57 +0300

    Some refactoring

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createBrowserHistory } from 'history';
 import { Provider } from 'react-redux';
 import { setupStore } from '../../redux/store';
 import App from './app';
-
+const history = createBrowserHistory();
 describe('App', () => {
   it('App should render properly', async () => {
     render(
@@ -16,5 +18,19 @@ describe('App', () => {
     expect(await screen.findByText(/curt z300/i)).toBeInTheDocument();
     expect(await screen.findAllByRole('link')).toHaveLength(20);
     expect(await screen.findAllByRole('button')).toHaveLength(9);
+  });
+
+  it('Should correctly response on bad url', async () => {
+    history.push('j/lkjdfo');
+    render(
+      <Provider store={setupStore()}>
+        <App/>
+      </Provider>);
+    expect(screen.getByText(/сильно старались, но не смогли найти запрашиваемую страницу/i)).toBeInTheDocument();
+    const link = screen.getByRole('link');
+    expect(link).toBeInTheDocument();
+    userEvent.click(link);
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+    expect(await screen.findByText(/честер bass/i)).toBeInTheDocument();
   });
 });
